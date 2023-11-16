cda79dfe132161ea8d1814c889865593bca45c01
shruti sanjna <shrutisanjna@gmail.com>
2023-11-15 23:03:15 -0800

    controller test

--- src/main/java/com/controller/AppController.java
+++ src/main/java/com/controller/AppController.java
@@ -3,6 +3,7 @@ package com.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.service.FeedbackService;
 
 @RestController
 @RequestMapping("/feedbackService")
+@CrossOrigin(origins = "http://localhost:3000/")
 public class AppController {
 
 	@Autowired
@@ -31,7 +33,7 @@ public class AppController {
 		return service.getAllUnacknowledgedFeeback();
 	}
 
-	@GetMapping("/getAacknowledge")
+	@GetMapping("/getAcknowledge")
 	private List<FeedbackEntity> getAllAcknowledgedFeeback() {
 		return service.getAllAcknowledgedFeeback();
 	}
